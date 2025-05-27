85b4e3cb7fc3f7b9b69fc4e9da46f1e82b9f7cb8
Gabriel Difforeni Leal <gleal0607@gmail.com>
2025-05-27 18:44:15 -0300

    Initial files for the project and camera classes

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gabiru/Documents/trab-FCG/GameEngine/src/glad.c" "CMakeFiles/main.dir/src/glad.c.o" "gcc" "CMakeFiles/main.dir/src/glad.c.o.d"
+  "/home/gabiru/Documents/trab-FCG/GameEngine/src/FreeCamera.cpp" "CMakeFiles/main.dir/src/FreeCamera.cpp.o" "gcc" "CMakeFiles/main.dir/src/FreeCamera.cpp.o.d"
+  "/home/gabiru/Documents/trab-FCG/GameEngine/src/LookAtCamera.cpp" "CMakeFiles/main.dir/src/LookAtCamera.cpp.o" "gcc" "CMakeFiles/main.dir/src/LookAtCamera.cpp.o.d"
+  "/home/gabiru/Documents/trab-FCG/GameEngine/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/gabiru/Documents/trab-FCG/GameEngine/src/textrendering.cpp" "CMakeFiles/main.dir/src/textrendering.cpp.o" "gcc" "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/src/FreeCamera.cpp.o.d
@@ -0,0 +1,176 @@
+CMakeFiles/main.dir/src/FreeCamera.cpp.o: \
+ /home/gabiru/Documents/trab-FCG/GameEngine/src/FreeCamera.cpp \
+ /usr/include/stdc-predef.h \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/FreeCamera.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/mat4x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/matrix_double4x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/type_mat4x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/type_vec4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/qualifier.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/setup.hpp \
+ /usr/include/c++/14/cassert \
+ /usr/include/c++/14/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /usr/include/c++/14/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/14/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/assert.h /usr/include/c++/14/cstddef \
+ /usr/lib/gcc/x86_64-redhat-linux/14/include/stddef.h \
+ /usr/include/c++/14/bits/version.h \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../simd/platform.h \
+ /usr/include/c++/14/cstdint \
+ /usr/lib/gcc/x86_64-redhat-linux/14/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/c++/14/type_traits \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/type_vec4.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/compute_vector_relational.hpp \
+ /usr/include/c++/14/limits \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/type_mat4x4.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../matrix.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/setup.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../vec2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_bool2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_vec2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_vec2.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_bool2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_float2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_float2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_double2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_double2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_int2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_int2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_uint2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_uint2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../vec3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_bool3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_vec3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_vec3.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_bool3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_float3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_float3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_double3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_double3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_int3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_int3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_uint3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_uint3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../vec4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_bool4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_bool4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_float4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_float4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_double4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_double4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/setup.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_int4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_int4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_uint4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/vector_uint4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat2x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double2x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat2x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat2x2.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double2x2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float2x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float2x2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat2x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double2x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat2x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat2x3.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double2x3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float2x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float2x3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat2x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double2x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat2x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat2x4.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double2x4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float2x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float2x4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat3x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double3x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat3x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat3x2.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double3x2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float3x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float3x2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat3x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double3x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat3x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat3x3.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double3x3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float3x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float3x3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat3x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double3x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat3x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat3x4.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double3x4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float3x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float3x4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat4x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double4x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat4x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat4x2.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double4x2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float4x2.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float4x2_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../mat4x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double4x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat4x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/../detail/type_mat4x3.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_double4x3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float4x3.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/.././ext/matrix_float4x3_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/func_matrix.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../geometric.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/func_geometric.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../exponential.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/type_vec1.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/type_vec1.inl \
+ /usr/include/c++/14/cmath /usr/include/c++/14/bits/requires_hosted.h \
+ /usr/include/c++/14/bits/cpp_type_traits.h \
+ /usr/include/c++/14/ext/type_traits.h /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h /usr/include/c++/14/bits/std_abs.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/sys/types.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/func_exponential.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/../vector_relational.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/../detail/setup.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/../detail/func_vector_relational.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/_vectorize.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../common.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/_fixes.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/func_common.inl \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/compute_common.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/../detail/../detail/../detail/../detail/setup.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/matrix_double4x4_precision.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/matrix_float4x4.hpp \
+ /home/gabiru/Documents/trab-FCG/GameEngine/include/glm/./ext/matrix_float4x4_precision.hpp
