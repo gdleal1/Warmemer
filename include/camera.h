a443e3198d1a0c77fda48c68458f2d013addc3d8
nadu <btoresan@gmail.com>
2025-06-04 12:31:07 -0300

    Me perdoa gabi <3 - mais modularizacao

---
+++ include/camera.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Variáveis que definem a câmera em coordenadas esféricas, controladas pelo
+// usuário através do mouse (veja função CursorPosCallback()). A posição
+// efetiva da câmera é calculada dentro da função main(), dentro do loop de
+// renderização.
+float g_CameraTheta = 0.0f; // Ângulo no plano ZX em relação ao eixo Z
+float g_CameraPhi = 0.0f;   // Ângulo em relação ao eixo Y
+float g_CameraDistance = 3.5f; // Distância da câmera para a origem
--- include/utils.h
+++ include/utils.h
@@ -17,13 +17,15 @@ static GLenum glCheckError_(const char *file, int line)
             case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
             case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
             case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+
             default:                               error = "UNKNOWN"; break;
         }
         fprintf(stderr, "ERROR: OpenGL \"%s\" in file \"%s\" (line %d)\n", error, file, line);
     }
     return errorCode;
 }
+
 #define glCheckError() glCheckError_(__FILE__, __LINE__)
 
-#endif // _UTILS_H
+#endif // _UTILS_
